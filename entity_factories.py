d830937e6ca48de019d1bad97d96ec38cc6574d9
Jose M. González <52630210+jmgonzalezro@users.noreply.github.com>
2023-09-01 14:35:38 +0200

    Add files via upload

---
+++ entity_factories.py
@@ -0,0 +1,48 @@
+from components.ai import HostileEnemy
+from components import consumable
+from components.fighter import Fighter
+from components.inventory import Inventory
+from entity import Actor, Item
+
+
+player = Actor(
+    char='@',
+    color=(255, 255, 255),
+    name='Player',
+    ai_cls=HostileEnemy,
+    fighter=Fighter(hp=30, defense=2, power=5),
+    inventory=Inventory(capacity=26)
+)
+
+
+orc = Actor(
+    char='o',
+    color=(63, 127, 63),
+    name='Orc',
+    ai_cls=HostileEnemy,
+    fighter=Fighter(hp=10, defense=0, power=3),
+    inventory=Inventory(capacity=0)
+)
+
+troll = Actor(
+    char='T',
+    color=(0, 127, 0),
+    name='Troll',
+    ai_cls=HostileEnemy,
+    fighter=Fighter(hp=16, defense=1, power=4),
+    inventory=Inventory(capacity=0)
+)
+
+health_potion = Item(
+    char='!',
+    color=(127, 0, 255),
+    name='Health Potion',
+    consumable=consumable.HealingConsumable(amount=4)
+)
+
+lightning_scrooll = Item(
+    char='~',
+    color=(255, 255, 0),
+    name='Lightning Scroll',
+    consumable=consumable.LightningDamageConsumable(20, maximum_range=5)
+)
